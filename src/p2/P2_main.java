2a52076e856b3230ac4284376a000aeca1070b3b
tutsi193 <tutsi.nc@outlook.com>
2025-10-30 13:36:30 +0100

    first commit

---
+++ src/p2/P2_main.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class P2_main {
+	public static void main(String[] args) {
+		int i = 1;
+		boolean b = false;
+		
+		if (i > 10) { 
+			b = true;
+		}
+		System.out.println("Wert von i:" + i);
+		System.out.println("Wert von b:" + b);
+	}
+	
+}
---
+++ src/p3/P3_main.java
@@ -0,0 +1,19 @@
+package p3;
+
+public class P3_main {
+	public static void main (String[] args) {
+		int i = 3;
+		int j = 4;
+		int k = 5;
+		
+		if (i < j) {
+			System.out.println("A");
+		}
+		if (i < k) {
+			System.out.println("B");
+		}
+		if (j < k) {
+			System.out.println("C");
+		}
+	}
+}
